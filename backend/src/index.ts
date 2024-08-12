8ca953881cf8271441264156813c963736114843
dalaixlmao <anubhav_a@me.iitr.ac.in>
2024-08-12 21:46:27 +0530

    updated index.ts backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 app.use('/user', userRoute);
 app.use('/admin', adminRoute);
 
+const PORT = process.env.PORT || 8000;
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port" + process.env.PORT);
